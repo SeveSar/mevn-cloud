e8d8df69c5a7810b5903afa70e5bf5d5af59a230
Аверин Артём <averin.dev@mail.ru>
2023-01-02 11:45:06 +0500

    change router middleware

--- src/router/middleware/auth.middleware.ts
+++ src/router/middleware/auth.middleware.ts
@@ -1,11 +1,12 @@
 import type { RouteLocationNormalized } from "vue-router";
 import { useAuthStore } from "@/store/auth";
-await useAuthStore().auth();
+
 export function authMiddleware(
   to: RouteLocationNormalized,
   from: RouteLocationNormalized
 ): void | { name: string } {
   const requiredAuth = to.matched.some((record) => record.meta.requiresAuth);
+  console.log(to);
   const userIsAuth = useAuthStore().isLoggedIn;
   if (requiredAuth && !userIsAuth) {
     return { name: "login" };
